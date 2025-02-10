3413e8db69fe9a58224c5b178a292780abdb2788
seonggon do <mooh2jj@naver.com>
2025-02-11 02:13:43 +0900

    :zap: Excel exception 오류 추가

---
+++ src/main/java/com/himedia/luckydokiapi/exception/ExcelFailException.java
@@ -0,0 +1,7 @@
+package com.himedia.luckydokiapi.exception;
+
+public class ExcelFailException extends RuntimeException {
+    public ExcelFailException(String message) {
+        super(message);
+    }
+}
