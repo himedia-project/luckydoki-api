77dbb8d407c3901da439881d5a6943cb46561d0e
y_daniel <y_daniel@naver.com>
2025-02-11 20:00:17 +0900

    :art: 커뮤니티상품 중간매핑 테이블 구현

--- src/main/java/com/himedia/luckydokiapi/domain/community/DTO/CommunityRequestDTO.java
+++ src/main/java/com/himedia/luckydokiapi/domain/community/DTO/CommunityRequestDTO.java
@@ -1,6 +1,5 @@
 package com.himedia.luckydokiapi.domain.community.DTO;
 
-import com.himedia.luckydokiapi.domain.member.entity.Member;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
@@ -9,7 +8,7 @@ import java.util.List;
 @Getter
 @SuperBuilder
 public class CommunityRequestDTO {
-    private String email;
+
     private String content;
     private List<String> imageList;
 }
