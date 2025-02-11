949f6f088d5c9b1094882a6ae34beaddf0a5e6df
y_daniel <y_daniel@naver.com>
2025-02-11 17:51:10 +0900

    몽고 설정 주석 처리

--- src/main/java/com/himedia/luckydokiapi/domain/chat/dto/ChatMessageDTO.java
+++ src/main/java/com/himedia/luckydokiapi/domain/chat/dto/ChatMessageDTO.java
@@ -1,23 +1,23 @@
-package com.himedia.luckydokiapi.domain.chat.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
-public class ChatMessageDTO {
-    private Long roomId;
-    private Long senderId;      // 메시지 보낸 사람의 Member ID
-    private String sellerEmail;      // 판매자 Member ID
-    private String buyerEmail;       // 구매자 Member ID
-    private Long productId;     // 상품 ID
-    private String message;     // 채팅 내용
-    private LocalDateTime sendDate;
-}
+//package com.himedia.luckydokiapi.domain.chat.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.time.LocalDateTime;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Builder
+//public class ChatMessageDTO {
+//    private Long roomId;
+//    private Long senderId;      // 메시지 보낸 사람의 Member ID
+//    private String sellerEmail;      // 판매자 Member ID
+//    private String buyerEmail;       // 구매자 Member ID
+//    private Long productId;     // 상품 ID
+//    private String message;     // 채팅 내용
+//    private LocalDateTime sendDate;
+//}
