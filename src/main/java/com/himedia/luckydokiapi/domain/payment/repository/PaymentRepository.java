0f80e0c98c4891f43956493e9b7a00bfcc9b0205
seonggon do <mooh2jj@naver.com>
2025-02-13 16:12:05 +0900

    :zap: 결제 모듈 진행 api 구현

---
+++ src/main/java/com/himedia/luckydokiapi/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,14 @@
+package com.himedia.luckydokiapi.domain.payment.repository;
+
+
+import com.himedia.luckydokiapi.domain.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("SELECT pm FROM Payment pm WHERE pm.orderId = :orderId")
+    Optional<Payment> findByOrderId(@Param("orderId") String orderId);
+}
