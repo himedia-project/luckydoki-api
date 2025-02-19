0c3f66369a34a087f597803559e59905d5b0648c
seonggon do <mooh2jj@naver.com>
2025-02-19 20:41:46 +0900

    :zap: data.sql 이벤트 insert 쿼리 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -55,4 +55,44 @@ INSERT INTO `category` (`id`, `last_type`, `logo`, `name`, `parent_id`) VALUES (
 
 
 
+INSERT INTO `event` (created_at, end_at, id, modified_at, start_at, content, image, title) VALUES ('2025-02-18 10:43:32', '2025-03-18', 7, '2025-02-18 10:43:32', '2025-02-18', '0시부터 24시까지, 딱 하루만!
+망설이는 순간 할인이 끝나요.
 
+최대 70% 할인에 무료배송 혜택까지 더했어요.
+지금 바로 득템의 행운을 누려보세요!', 's_859400be-37e9-455b-8503-cc7745f2de62-todayevent.jpg', '오늘만 할인');
+INSERT INTO `event` (created_at, end_at, id, modified_at, start_at, content, image, title) VALUES ('2025-02-18 11:43:48', '2025-03-12', 8, '2025-02-18 11:43:48', '2025-02-12', '1020 새내기들을 위한 신학기 할인전이 시작되었어요.
+가방부터 문구, 디지털 액세사리까지!
+최대 78%까지의 할인 혜택을 만나보세요.
+d+ 멤버십에 가입하면 추가 할인 혜택도 받을 수 있어요!', 's_ace1490b-1425-43be-9f96-f0dfa7d21034-star.jpg', '별작가님의 여정에 함께 동참해 보실래요~?');
+INSERT INTO `event` (created_at, end_at, id, modified_at, start_at, content, image, title) VALUES ('2025-02-18 11:46:38', '2025-03-12', 10, '2025-02-18 11:46:38', '2025-02-12', '속은 든든하지만 실제로는 가벼운
+식단관리 추천템으로 올해는 꼭 성공해보세요.
+맛있으면서 포만감까지 채울 수 있는 수제 먹거리로
+건강한 식단 관리를 완성할 수 있어요.', 's_f52a9c8a-a00a-4867-a608-7fff8f94db4c-food.jpg', '지금 시작해요! 올해의 식단관리');
+INSERT INTO `event` (created_at, end_at, id, modified_at, start_at, content, image, title) VALUES ('2025-02-18 11:49:12', '2025-03-17', 11, '2025-02-18 11:49:12', '2025-02-12', '1020 새내기들을 위한 신학기 할인전이 시작되었어요.
+가방부터 문구, 디지털 액세사리까지!
+최대 78%까지의 할인 혜택을 만나보세요.
+d+ 멤버십에 가입하면 추가 할인 혜택도 받을 수 있어요!', 's_dcd67b8f-b7e2-4f0b-8cc8-5be02de27502-newbee.jpg', '새내기들을 위한 신학기 할인전');
+INSERT INTO `event` (created_at, end_at, id, modified_at, start_at, content, image, title) VALUES ('2025-02-18 11:51:11', '2025-03-10', 12, '2025-02-18 11:51:11', '2025-02-06', '우리 아이들의 어린이집, 유치원, 학교 등에서의
+즐거운 마무리, 졸업식 시즌이 돌아왔습니다!
+이색꽃다발부터 토퍼, 이벤트, 단체선물까지
+Best 졸업 선물 작품과 함께 축하해요.', 's_033748f0-dd79-4e7c-bd6f-c3e303614eb3-graduate.jpg', '졸업의 주인공을 더 찬란하고 빛나게');
+
+
+
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 10:43:31.835581', 7, 2, 231);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 10:43:31.849705', 7, 3, 230);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 10:43:31.855937', 7, 4, 227);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 10:43:31.861254', 7, 5, 222);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:43:48.497369', 8, 6, 247);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:43:48.504006', 8, 7, 245);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:43:48.507631', 8, 8, 233);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:43:48.511716', 8, 9, 229);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:46:38.069898', 10, 13, 228);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:46:38.073849', 10, 14, 229);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:46:38.078525', 10, 15, 231);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:49:11.603901', 11, 16, 204);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:49:11.607581', 11, 17, 189);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:49:11.610392', 11, 18, 170);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:51:11.052629', 12, 19, 233);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:51:11.056827', 12, 20, 231);
+INSERT INTO luckydoki.event_bridge (created_at, event_id, id, product_id) VALUES ('2025-02-18 11:51:11.062643', 12, 21, 172);
