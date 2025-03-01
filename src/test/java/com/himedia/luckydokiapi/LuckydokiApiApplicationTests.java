331f654e0425118369497853bc381c2b999f8694
seonggon do <mooh2jj@naver.com>
2025-03-01 21:43:55 +0900

    :memo: 테스트코드 주석처리

--- src/test/java/com/himedia/luckydokiapi/LuckydokiApiApplicationTests.java
+++ src/test/java/com/himedia/luckydokiapi/LuckydokiApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.himedia.luckydokiapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LuckydokiApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class LuckydokiApiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
