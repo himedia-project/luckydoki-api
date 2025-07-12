de2b41847090c44df78f5cbe01ab56ac6be1b8ab
seonggon do <mooh2jj@naver.com>
2025-07-12 11:52:47 +0900

    :memo: 엑셀 주석 내용 추가

--- src/main/java/com/himedia/luckydokiapi/util/excel/ExcelGenerator.java
+++ src/main/java/com/himedia/luckydokiapi/util/excel/ExcelGenerator.java
@@ -15,6 +15,13 @@ import java.util.List;
 
 public class ExcelGenerator {
 
+    /**
+     * 엑셀 파일을 생성합니다.
+     * @param data 엑셀에 포함될 데이터 리스트
+     * @param sheetName 엑셀 시트 이름
+     * @return ByteArrayResource 엑셀 파일의 ByteArrayResource
+     * @param <T> 데이터 타입
+     */
     public static <T> ByteArrayResource generateExcelFile(List<T> data, String sheetName) {
         try {
             SimpleExcelFile<T> userInfoFile = new SimpleExcelFile<>(data, sheetName);
@@ -26,6 +33,12 @@ public class ExcelGenerator {
         }
     }
 
+    /**
+     * ByteArrayResource를 사용하여 ResponseEntity를 생성합니다.
+     * @param byteArrayResource 엑셀 파일의 ByteArrayResource
+     * @param fileName 다운로드할 파일 이름
+     * @return ResponseEntity<ByteArrayResource> 객체
+     */
     public static ResponseEntity<ByteArrayResource> createResponseEntity(ByteArrayResource byteArrayResource, String fileName) {
         return ResponseEntity.ok()
                 .headers(createHttpHeaders(fileName))
@@ -33,6 +46,11 @@ public class ExcelGenerator {
                 .body(byteArrayResource);
     }
 
+    /**
+     * HTTP 헤더를 생성합니다.
+     * @param fileName 다운로드할 파일 이름
+     * @return 생성된 HttpHeaders 객체
+     */
     private static HttpHeaders createHttpHeaders(String fileName) {
         HttpHeaders headers = new HttpHeaders();
         // headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
